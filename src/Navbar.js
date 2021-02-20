2e0a85141b8b937bbc1acc5247a099a34bb16429
kamdjouduplex <tony14pro@gmail.com>
2021-02-20 16:50:44 +0100

    imbrequer les componsantes

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    
+  return (
+    <nav className="navbar">
+      <div>
+        <a href="">Bproo Blog</a>
+      </div>
+      <ul className="liens">
+        <li>
+          <a href="" className="lien">Acceuill</a>
+        </li>
+        <li>
+          <a href="" className="lien">Creer Article</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
