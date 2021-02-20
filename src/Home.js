9f8eb787c83857039354259949746b2d9df725d2
kamdjouduplex <tony14pro@gmail.com>
2021-02-20 18:17:06 +0100

    Gestion des evenements dans react

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Vous m'avez cliker...");
+    };
+
+    const handleClickAgain = (name, event) => {
+        console.log(`Salut, ${name}`, event.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Notre page d'acceuil</h2>
+            <button onClick={handleClick}>Clicker moi!</button>
+            <button onClick={ (e) => handleClickAgain('Duplex', e) }>Clicker moi encore</button>
         </div>
      );
 }
