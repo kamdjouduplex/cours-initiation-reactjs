12051442d59a409fe5ba6f574868272a79567908
kamdjouduplex <tony14pro@gmail.com>
2021-03-03 15:50:16 +0100

    added the the loading condition

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,24 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlog] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect( ()=> {
-        fetch('http://localhost:8000/blogs')
+        setTimeout( () => {
+            fetch('http://localhost:8000/blogs')
             .then( (response) => {
                 return response.json();
             })
             .then( (data) => {
                 setBlog(data);
+                setIsLoading(false);
             })
+        },2000)
     }, []);
 
     return ( 
         <div className="home">
+            { isLoading && <div>En cour de traitement ...</div> }
             { blogs && <BlogList blogs={blogs} title={'Liste des blogs'} /> }
         </div>
      );
