9741e4ea6b97f597fa7295ff1d48f4029a9638bc
kamdjouduplex <tony14pro@gmail.com>
2021-03-03 13:38:51 +0100

    useEffet dans react lecon-14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -31,6 +31,11 @@ const Home = () => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id);
         setBlog(newBlogs);
     }
+
+    useEffect( ()=> {
+        console.log('Use Effet a ete appele...');
+        console.log(blogs);
+    });
 
     return ( 
         <div className="home">
