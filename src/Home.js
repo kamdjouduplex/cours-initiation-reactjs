e1563942cc4c4b610ebbb667807c6b8237307d47
kamdjouduplex <tony14pro@gmail.com>
2021-03-03 16:37:58 +0100

    gestion des erreur

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,33 @@ const Home = () => {
 
     const [blogs, setBlog] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect( ()=> {
         setTimeout( () => {
             fetch('http://localhost:8000/blogs')
-            .then( (response) => {
-                return response.json();
-            })
-            .then( (data) => {
-                setBlog(data);
-                setIsLoading(false);
-            })
+                .then( (response) => {
+                    if(!response.ok){
+                        throw Error('Desole une erreur est survenu...');
+                    }
+                    return response.json();
+                })
+                .then( (data) => {
+                    setBlog(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch( err => {
+                    console.log(err.message);
+                    setError(err.message);
+                    setIsLoading(false);
+                })
         },2000)
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div style={ {'color': 'red'}}>{error}</div>} 
             { isLoading && <div>En cour de traitement ...</div> }
             { blogs && <BlogList blogs={blogs} title={'Liste des blogs'} /> }
         </div>
