b13783ad83ca696f726b0f8b6e0747f8e25343f9
kamdjouduplex <tony14pro@gmail.com>
2021-03-03 14:54:52 +0100

    adding json-server

--- src/Home.js
+++ src/Home.js
@@ -27,8 +27,6 @@ const Home = () => {
           }
     ]);
 
-    const [name, setName] = useState('Duplex');
-
     const HandleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id);
         setBlog(newBlogs);
@@ -36,14 +34,11 @@ const Home = () => {
 
     useEffect( ()=> {
         console.log('Use Effet a ete appele...');
-        console.log(blogs);
-    }, [name]);
+    }, []);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title={'Liste des blogs'} HandleDelete={HandleDelete} />
-            <button onClick={ () => setName('Tony') }>change le nom</button>
-            <p>{name}</p>
         </div>
      );
 }
