c23188037c2db47c402c741f90266189829b9887
kamdjouduplex <tony14pro@gmail.com>
2021-03-11 17:22:22 +0100

    adding the Link component to route

---
+++ src/Ajouter.js
@@ -0,0 +1,9 @@
+const Ajouter = () => {
+    return ( 
+        <div className="create">
+            <h2>Ajouter un nouveau article</h2>
+        </div>
+     );
+}
+ 
+export default Ajouter;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Ajouter from './Ajouter';
 
 function App() {
 
@@ -11,8 +12,11 @@ function App() {
         <Navbar />
         <div className="contenu">
           <Switch>
-            <Route path={'/'}>
+            <Route exact path={'/'}>
               <Home />
+            </Route>
+            <Route path={'/ajouter'}>
+              <Ajouter />
             </Route>
           </Switch>
         </div>
